109c9adaa13a0b20895d05cb98cbc6078472e089
Dino Duratović <dinomol@mail.com>
2021-03-06 01:50:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Falcon Briefing
+Utility to show the briefing files from Falcon BMS on your smartphone,
+tablet or any device running a web browser.
+
+#### How to use
+1. Run the `falcon-briefing.py` Python file.
+2. Click the "Print" button when you are viewing a briefing inside Falcon BMS
+3. Open the address shown in the `falcon-briefing.py` window (Example: `http://192.168.1.100:8000/current-briefing.html`) with your smartphone's browser.
+
+For it to work, you have to enable the "Briefing Output to File" and
+"HTML Briefings" options in Falcon BMS.
+
+#### Dependencies
+* Python 3
+* [Watchgod (Python module)](https://github.com/samuelcolvin/watchgod)
+
+#### Tip for Usage
+
+I compiled the script into a single-file executable for use on Windows
+using [PyInstaller](https://www.pyinstaller.org/). I placed the file somewhere on my disk and automatically start it whenever I open Falcon BMS. Then I also created a shortcut to the address the briefing is server at on my phone and placed it on the home screen.
+
+That way all I ever have to do is start Falcon BMS, click on the "Print" when I view the briefing and click on the icon on my smartphone's home screen.
+
+**Notice: PyInstaller compiled Windows executables are sometimes falsely tagged as malware by various malware tools. See [this](https://github.com/pyinstaller/pyinstaller/issues?q=is%3Aissue+virus+is%3Aclosed), [this](https://stackoverflow.com/questions/43777106/program-made-with-pyinstaller-now-seen-as-a-trojan-horse-by-avg) and [this](https://www.reddit.com/r/Python/comments/9ri81s/my_pyinstallercompiled_exe_progs_are_victims_of/).**
+
+License: [GPLv3](http://www.gnu.org/licenses/gpl-3.0.html)  
+Icon: [Clipboard, document icon](https://www.iconfinder.com/icons/1055091/clipboard_document_icon) by Nick Roach; licensed under the [GPLv3](http://www.gnu.org/licenses/gpl-3.0.html)
---
+++ falcon-briefing.py
@@ -0,0 +1,74 @@
+# Falcon Briefing
+# Copyright (C) 2021 Dino Duratović <dinomol at mail dot com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+try:
+    from watchgod import watch, Change
+except ImportError:
+    print("Watchgod module missing. Please install watchgod")
+    sys.exit(1)
+
+import http.server
+import socket
+import socketserver
+import os
+import os.path
+import sys
+import threading
+import winreg
+
+PORT = 8000
+LOCAL_IP = socket.gethostbyname(socket.gethostname())
+
+def get_falcon_path():
+    reg = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
+    try:
+        key = winreg.OpenKey(reg, r"SOFTWARE\WOW6432Node\Benchmark Sims\Falcon BMS 4.35")
+        return winreg.QueryValueEx(key, "baseDir")[0]
+    except FileNotFoundError:
+        print("Cannot find the Falcon BMS path in Windows Registry. Install broken?")
+        sys.exit(1)
+
+def remove_old_briefings(briefing_path):
+    for f in os.listdir(briefing_path):
+        if f.endswith(".html"):
+            os.remove(os.path.join(briefing_path, f))
+
+def run_http_server(path):
+    # runs a HTTP server in the background, which serves the briefing files
+    def http_server():
+        Handler = http.server.SimpleHTTPRequestHandler
+        httpd = socketserver.TCPServer(("", PORT), Handler)
+        print("Briefings are served at http://{}/current-briefing.html".format(LOCAL_IP))
+        httpd.serve_forever()
+
+    os.chdir(path)
+    thread = threading.Thread(target=http_server, daemon=True)
+    thread.start()
+
+def watch_briefings(briefing_path):
+    # waits for new files in the directory and renames every new file
+    # to "current-briefing.html", which can then be opened by a browser
+    for changes in watch(briefing_path):
+        for change, path in changes:
+            if change == Change.added:
+                os.replace(path, os.path.join(briefing_path, "current-briefing.html"))
+
+falcon_path = get_falcon_path()
+briefing_path = "{}\\User\\Briefings".format(falcon_path)
+
+remove_old_briefings(briefing_path)
+run_http_server(briefing_path)
+watch_briefings(briefing_path)
